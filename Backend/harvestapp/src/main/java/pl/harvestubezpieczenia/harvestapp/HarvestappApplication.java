b46595b226da9690b8b7212ff5c23f0daf5be821
s17073 <s17073@pjwstk.edu.pl>
2024-07-16 14:53:29 +0200

    init backend

---
+++ Backend/harvestapp/src/main/java/pl/harvestubezpieczenia/harvestapp/HarvestappApplication.java
@@ -0,0 +1,13 @@
+package pl.harvestubezpieczenia.harvestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestappApplication.class, args);
+	}
+
+}
---
+++ Backend/harvestapp/src/test/java/pl/harvestubezpieczenia/harvestapp/HarvestappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.harvestubezpieczenia.harvestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
