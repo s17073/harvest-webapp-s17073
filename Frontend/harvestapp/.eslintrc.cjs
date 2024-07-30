fe97d97f323f95519603a089bc42c06e144c82d9
s17073 <s17073@pjwstk.edu.pl>
2024-07-30 06:40:39 +0200

    Config

--- Frontend/harvestapp/.eslintrc.cjs
+++ Frontend/harvestapp/.eslintrc.cjs
@@ -17,5 +17,8 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts"] }],
   },
 };
