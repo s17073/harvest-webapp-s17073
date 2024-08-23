1435cface2b447c73184809c1393a54e1017f08d
s17073 <s17073@pjwstk.edu.pl>
2024-08-23 17:34:50 +0200

    Cors config

--- Backend/harvestapp/src/main/java/pl/harvestubezpieczenia/harvestapp/config/WebConfig.java
+++ Backend/harvestapp/src/main/java/pl/harvestubezpieczenia/harvestapp/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
 }
