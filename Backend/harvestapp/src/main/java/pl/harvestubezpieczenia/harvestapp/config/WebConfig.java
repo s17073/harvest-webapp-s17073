2fec2d91d11f1c73a1c92ef444d892fea8f27b10
s17073 <s17073@pjwstk.edu.pl>
2024-08-23 17:21:54 +0200

    Cors config

--- Backend/harvestapp/src/main/java/pl/harvestubezpieczenia/harvestapp/config/WebConfig.java
+++ Backend/harvestapp/src/main/java/pl/harvestubezpieczenia/harvestapp/config/WebConfig.java
@@ -1,15 +1,17 @@
 package pl.harvestubezpieczenia.harvestapp.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("Get", "Post", "Put", "Delete")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
